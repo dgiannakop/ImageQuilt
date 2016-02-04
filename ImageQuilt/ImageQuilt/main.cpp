1dec6caa00f76eaca1e258e2a2e96e3a2a5263df
Dimitrios Giannakopoulos <venom.gr@gmail.com>
2016-02-04 13:07:39 +0100

    Updated sample output

--- ImageQuilt/ImageQuilt/main.cpp
+++ ImageQuilt/ImageQuilt/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	ImageQuilt* image_quilt = new ImageQuilt("../input/text.bmp", "../output/output.bmp", 64, 4, 16, 0.1);
+	ImageQuilt* image_quilt = new ImageQuilt("../input/text.bmp", "../output/output.bmp", 64, 8, 16, 0.1);
 	image_quilt->synthesize();
 	delete image_quilt;
 	return 0;
